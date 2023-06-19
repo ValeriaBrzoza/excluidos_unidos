a523090bd9502b333f69192097282a738019ca00
Brubrux <bortielli@fi.uba.ar>
2023-06-19 08:02:15 -0300

    agregue vista config, y separe la vista listas

---
+++ lib/screens/Views/lists_view.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:prueba_01/screens/Views/tasklist_creator_view.dart';
+import 'package:prueba_01/services/falsedataprovider.dart';
+import 'package:prueba_01/screens/Views/tasklist_view.dart';
+import 'package:prueba_01/models/tasklist.dart';
+
+class ListsView extends StatefulWidget {
+  const ListsView({super.key});
+
+  @override
+  State<ListsView> createState() => _ListsViewState();
+}
+
+class _ListsViewState extends State<ListsView> {
+  List<TaskList> listas = [];
+  int selectedIndex = 0;
+
+  @override
+  void initState() {
+    listas = DataProvider.instance.getLists();
+    super.initState(); //"BASE DE DATOS" obteniendo las listas
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //devuelve vista
+      appBar: AppBar(
+          //vista tiene barrita arriba
+          title: const Text('Mis listas de tareas'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+            ),
+          ]),
+      floatingActionButton: FloatingActionButton(
+        //boton de añadir lista
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) =>
+                  const TaskListCreatorView()); //formulario de creacion de listas
+        },
+        child: const Icon(Icons.add), //icono del boton
+      ),
+      //vista de listas
+      body: ListView.builder(
+        //recibe lista de widgets y los muestra en orden scroleable
+        itemCount: listas.length,
+        itemBuilder: (context, index) => ListTile(
+          //elemento de la lista con formato de item
+          title: Text(listas[index].name),
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => TaskListView(id: index.toString())));
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:prueba_01/screens/tasklist_creator_view.dart';
-import 'package:prueba_01/services/falsedataprovider.dart';
-import 'package:prueba_01/screens/tasklist_view.dart';
+import 'package:prueba_01/screens/Views/lists_view.dart';
+import 'package:prueba_01/screens/Views/settings_screen.dart';
 import 'package:prueba_01/models/tasklist.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,68 +12,44 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<TaskList> listas = [];
-
-  @override
-  void initState() {
-    listas = DataProvider.instance.getLists();
-    super.initState(); //"BASE DE DATOS" obteniendo las listas
-  }
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    final screens = [
+      const ListsView(),
+      const SettingsView(),
+    ];
+    final themeColors = Theme.of(context).colorScheme;
+
     return Scaffold(
-      //devuelve vista
-      appBar: AppBar(
-          //vista tiene barrita arriba
-          title: const Text('Mis listas de tareas'),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.search),
-            ),
-          ]),
-      floatingActionButton: FloatingActionButton(
-        //boton de añadir lista
-        onPressed: () {
-          showDialog(context: context, builder: (context) => const TaskListCreatorView()); //te lleva a otra vista
-          // Navigator.of(context).push(AdaptativeModalPageRoute(width: width, builder: (context) => TaskListCreatorView()));
-        },
-        child: const Icon(Icons.add), //icono del boton
+      body: IndexedStack(
+        index: selectedIndex,
+        children: screens,
       ),
       //Botones de la Barra de navegacion
-      bottomNavigationBar: NavigationBar(
-        //barrita de abajo
-        destinations: const [
-          //NavigationBar va a tener logica de navergacion
-          NavigationDestination(
-            //dibujos de abajo
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+        fixedColor: themeColors.secondary,
+        backgroundColor: themeColors.primary,
+        unselectedItemColor: themeColors.background,
+        items: const [
+          BottomNavigationBarItem(
             icon: Icon(Icons.list),
             label: 'My Lists',
           ),
-          NavigationDestination(
+          BottomNavigationBarItem(
             //dibujos de abajo
             icon: Icon(Icons.settings),
             label: 'Settings',
           )
         ],
       ),
-      //vista de listas
-      body: ListView.builder(
-        //recibe lista de widgets y los muestra en orden scroleable
-        itemCount: listas.length,
-        itemBuilder: (context, index) => ListTile(
-          //elemento de la lista con formato de item
-          title: Text(listas[index].name),
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => TaskListView(id: index.toString())));
-          },
-        ),
-      ),
     );
   }
 }
-
-
-          // itemCount: 1,
-          // itemBuilder: (context, index) =>
-          //     ListTile(title: Text('Lista nro $index')),
