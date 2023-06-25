113e9c5fcf6cdb2f6c05147614e0593bde824abc
Valeria Brzoza <vbrzoza@fi.uba.ar>
2023-06-25 16:38:01 -0300

    Comentarios en la parte de notificaciones

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 
-const firebase = require("firebase-admin");
-firebase.initializeApp()
+const firebase = require("firebase-admin"); // importa la libreria de firebase
+firebase.initializeApp() // inicializa la app de firebase
 
 const functions = require("firebase-functions");  // require funciona como importar. Importa las funciones de firebase
 const firestore = firebase.firestore()
@@ -12,18 +12,32 @@ const {
     onDocumentDeleted,
     Change,
     FirestoreEvent
-} = require("firebase-functions/v2/firestore")
+} = require("firebase-functions/v2/firestore") // importa las funciones de firestore
 
+
+async function sendNotificationToUser(userId, notification) {
+    const tokenDocs = await firestore.collection("fcm_tokens").where("user_id", "==", userId).get() //busco en la coleccion fcm_tokens el token del usuario
+    const tokens = tokenDocs.docs.map(doc => doc.id) //lo vuelvo un array de strings (el id del documento es el token)
+
+    functions.logger.info(`Notificación enviada: ${JSON.stringify(notification)}`, {structuredData: true}); //muestra en la consola de firebase el mensaje
+
+    for (const token of tokens) {
+        await firebase.messaging().send({ //envia al token la notificaion con el titulo y el cuerpo
+            "token": token,
+            "notification": notification
+        })
+    }
+}
 
 module.exports.listCreated = onDocumentCreated('task_lists/{id}', async (event) => {
-    const data = event.data.data()
+    const data = event.data.data() //event es lo que paso "crear una lista", .data es el documento y .data() es el contenido del documento
     const users = data.users
 
     const deadline = data.global_deadline
 
-    functions.logger.info(`Lista creada: ${data.name}, usuarios: ${users}, deadline ${JSON.stringify(deadline)}`, {structuredData: true});
+    functions.logger.info(`Lista creada: ${data.name}, usuarios: ${users}, deadline ${deadline}`, {structuredData: true}); //muestra en la consola de firebase el mensaje
 
-    for (const user of users) {
+    for (const user of users) { //recorre los usuarios con los que esta compartido
         await sendNotificationToUser(user, {
             "title": `Nueva lista: ${data.name}`,
             "body": deadline ? `Fecha límite: ${(new Date(deadline)).toLocaleDateString()}` : "Sin fecha límite"
@@ -50,16 +64,3 @@ module.exports.listTaskCreated = onDocumentCreated('task_lists/{id}/tasks/{taskI
 });
 
 
-async function sendNotificationToUser(userId, notification) {
-    const tokenDocs = await firestore.collection("fcm_tokens").where("user_id", "==", userId).get()
-    const tokens = tokenDocs.docs.map(doc => doc.id)
-
-    functions.logger.info(`Notificación enviada: ${JSON.stringify(notification)}`, {structuredData: true});
-
-    for (const token of tokens) {
-        await firebase.messaging().send({ //envia al token la notificaion con el titulo y el cuerpo
-            "token": token,
-            "notification": notification
-        })
-    }
-}
